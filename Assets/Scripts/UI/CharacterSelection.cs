e1786333902f327ec450a39241246e14b0f0bd1c
Santino Casolati <santicasolati@gmail.com>
2025-09-04 19:39:00 -0300

    Character Selection added

---
+++ Assets/Scripts/UI/CharacterSelection.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelection : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> characterSprites;
+    [SerializeField] private Image characterImg;
+    [SerializeField] private Button prevBtn;
+    [SerializeField] private Button nextBtn;
+
+    private int currentIndex = 0;
+
+    private void Awake()
+    {
+        SetCharacter(0);
+    }
+
+    private void OnEnable()
+    {
+        prevBtn.onClick.AddListener(() => SetCharacter(-1));
+        nextBtn.onClick.AddListener(() => SetCharacter(1));
+    }
+
+    private void OnDisable()
+    {
+        prevBtn.onClick.RemoveAllListeners();
+        nextBtn.onClick.RemoveAllListeners();
+    }
+
+    private void SetCharacter(int direction)
+    {
+        int updatedIndex = currentIndex + direction;
+
+        if (updatedIndex >= characterSprites.Count)
+            updatedIndex = 0;
+        else if (updatedIndex < 0)
+            updatedIndex = characterSprites.Count - 1;
+
+        characterImg.sprite = characterSprites[updatedIndex];
+
+        currentIndex = updatedIndex;
+    }
+}
